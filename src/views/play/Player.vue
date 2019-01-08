04947c5c53f5b7142ef6a60e06df2d17fecc9313
code-mcx <1827939333@qq.com>
2019-01-08 18:32:27 +0800

    Fix first play clicked :clap:

--- src/views/play/Player.vue
+++ src/views/play/Player.vue
@@ -145,7 +145,7 @@
       playOrPause() {
         if (this.playStatus === false) {
           // 表示第一次播放
-          if (this.first === undefined) {
+          if (this.first === undefined && !this.audioDOM.src) {
             this.audioDOM.src = this.song.url;
             this.first = true;
           }
